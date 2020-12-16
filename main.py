03b428a6d47df904b26111f8ec0c8c3636bdcc45
Witek131 <wik131@rambler.ru>
2020-12-16 14:48:38 +0300

    Update main.py
    new3

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print('YELLLO')
     print('sfdsdgf')
+    print('asdas')
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
