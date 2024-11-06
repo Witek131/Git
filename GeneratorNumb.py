c07cb7b5c85055998c1fbc62415e8130a14abdfd
Witek131 <wik131@rambler.ru>
2024-11-06 18:00:34 +0300

    Генератор

--- GeneratorNumb.py
+++ GeneratorNumb.py
@@ -1,4 +1,25 @@
-def generate(n: int, m: int, prefix=None):  # n чмслa m длинна
+def findIn(n, pr):  # ищет n в pr
+    for x in pr:
+        if x == n:
+            return True
+    return False
+
+
+def generate_pemutations(n: int, m=-1, pref=None):  # генерация n чисел в m позициях
+    m = n if m == -1 else m
+    pref = pref or []
+    if m == 0:
+        print(pref)
+        return
+    for i in range(1, n + 1):
+        if findIn(i, pref):
+            continue
+        pref.append(i)
+        generate_pemutations(n, m - 1, pref)
+        pref.pop()
+
+
+def generate(n: int, m: int, prefix=None):  # генерация n чисел m длинной
     if m == 0:
         print(prefix)
         return
@@ -7,4 +28,8 @@ def generate(n: int, m: int, prefix=None):  # n чмслa m длинна
         prefix.append(i)
         generate(n, m - 1, prefix)
         prefix.pop()
-generate(3, 5)
+
+
+# generate(2, 3)
+print(11111)
+generate_pemutations(6, 6)
